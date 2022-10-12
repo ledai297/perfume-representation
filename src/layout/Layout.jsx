d7a6af22b9a90abde1f8b2052ccb068638cca89a
Matthew <matthew@dstarter.io>
2022-10-12 17:19:11 +0700

    Fix bug 20221012

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -12,6 +12,12 @@ export const Layout = ({ children }) => {
         {children}
         <Insta />
       </main>
+      <div className='facebook-logo-fixed shake' onClick={() => window.open('https://www.facebook.com/ribicosmetics', "_target")}>
+        <img src="/assets/img/fb_logo.webp"/>
+      </div>
+      <div className='zalo-logo-fixed shake' onClick={() => window.open('tel:0907199863')}>
+        <img src="/assets/img/callnow.png"/>
+      </div>
       <div className='shopee-logo-fixed shake' onClick={() => window.open('https://shopee.vn/ribicosmetics', "_target")}>
         <img src="/assets/img/shopee-logo.png"/>
       </div>
